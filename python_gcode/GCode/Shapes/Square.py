9bc71df4ced545427d9fbfbc0b715ee9cb215de1
root <root+CNC3018@n5010-LINUX>
2018-07-07 18:11:26 -0400

    jed@n5010: Sat Jul  7 22:11:26 UTC 2018

---
+++ python_gcode/GCode/Shapes/Square.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from .. import Line
+
+import numpy as np
+
+class Square(Line):
+    def __init__(
+        self, len_side=10, origin=np.array([0, 0]), rotation=0, *args, **kwargs
+    ):
+        self.len_side = len_side
+        self.origin = origin
+
+        kwargs["points"] = self._points
+
+        super().__init__(*args, **kwargs)
+
+    @property
+    def _points(self):
+        return np.array(
+            [
+                [self.origin[0], self.origin[1]],
+                [self.origin[0] + self.len_side, self.origin[1]],
+                [
+                    self.origin[0] + self.len_side,
+                    self.origin[1] + self.len_side,
+                ],
+                [self.origin[0], self.origin[1] + self.len_side],
+                [self.origin[0], self.origin[1]],
+            ]
+        )
+
+    def generate_gcode(self):
+        self.points = self._points
+        super().generate_gcode()
+
+    @property
+    def _cls(self):
+        return self.__class__.__name__
+
+    def __repr__(self):
+        return "{}<O={}, L={}>".format(self._cls, self.origin, self.len_side)
---
+++ python_gcode/GCode/Shapes/__init__.py
@@ -0,0 +1,1 @@
+from .Square import Square
