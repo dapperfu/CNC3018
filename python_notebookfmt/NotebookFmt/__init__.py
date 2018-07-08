5c510ef91daa6b74a7f28b5e18f76209915a7076
root <root+CNC3018@n5010-LINUX>
2018-07-07 23:43:45 -0400

    jed@n5010: Sun Jul  8 03:43:45 UTC 2018

--- python_notebookfmt/NotebookFmt/__init__.py
+++ python_notebookfmt/NotebookFmt/__init__.py
@@ -12,7 +12,7 @@ from .clean_empty_notebook_cells import clean_empty_notebook_cells
 
 from .isort_notebook_cells import isort_notebook_cells
 
-from .black_import_cells import black_import_cells
+from .black_notebook_cells import black_notebook_cells
 from .clean_empty_notebook_cells import bubble_import_notebook_cells
 
 
