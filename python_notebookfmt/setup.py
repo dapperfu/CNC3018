ec49729d17f208cad0e5a8ef6bfac27779d4a739
root <root+CNC3018@n5010-LINUX>
2018-07-07 23:02:46 -0400

    jed@n5010: Sun Jul  8 03:02:46 UTC 2018

--- python_notebookfmt/setup.py
+++ python_notebookfmt/setup.py
@@ -9,6 +9,9 @@ setup(
     packages=["NotebookFmt"],
     entry_points = {
         'console_scripts': ['notebookformat=NotebookFmt.__init__:main'],
+        'snek_types': [
+            'cute = cute_snek:cute_snek',
+        ],
     },
     zip_safe=False,
 )
