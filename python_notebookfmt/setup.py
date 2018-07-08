4b1c6dfba44238619bb70c58019c482df645a0e0
root <root+CNC3018@n5010-LINUX>
2018-07-07 22:44:46 -0400

    jed@n5010: Sun Jul  8 02:44:46 UTC 2018

---
+++ python_notebookfmt/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="NotebookFmt",
+    version="0.1",
+    description="NotebookFmt",
+    author="Jed Frey",
+    license="BSD",
+    packages=["NotebookFmt"],
+    entry_points = {
+        'console_scripts': ['notebookformat=NotebookFmt.__init__:main'],
+    },
+    zip_safe=False,
+)
