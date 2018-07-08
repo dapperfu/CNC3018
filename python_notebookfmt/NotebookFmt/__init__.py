cd0d94ced727a3e5629a7329a6dd4894d1a33d5c
root <root+CNC3018@n5010-LINUX>
2018-07-07 23:45:24 -0400

    jed@n5010: Sun Jul  8 03:45:24 UTC 2018

--- python_notebookfmt/NotebookFmt/__init__.py
+++ python_notebookfmt/NotebookFmt/__init__.py
@@ -7,16 +7,14 @@ from .clean_empty_notebook_cells import clean_empty_notebook_cells
 from .isort_notebook_cells import isort_notebook_cells
 
 from .black_notebook_cells import black_notebook_cells
-from .clean_empty_notebook_cells import bubble_import_notebook_cells
+from .bubble_import_notebook_cells import bubble_import_notebook_cells
 
-
+# TODO: Entry points.
 cleaning_functions = list()
-
 cleaning_functions.append(bubble_import_notebook_cells)
 cleaning_functions.append(isort_notebook_cells)
 cleaning_functions.append(black_notebook_cells)
 cleaning_functions.append(clean_empty_notebook_cells)
-
 
 def main(args=sys.argv):
     assert len(args) == 2
