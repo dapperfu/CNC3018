00f0a50cf8a00939196f18089288232c29d1f595
root <root+CNC3018@n5010-LINUX>
2018-07-07 23:49:40 -0400

    Prelazy

--- lazy.sh
+++ lazy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Make sure your venv is not a subfolder of where this is run.
+git commit -am "Prelazy"
 find . -name "*.py" | xargs -n1 -P8 black --py36 --line-length 80
+find . -name "*.ipynb" | xargs -n1 -P8 notebookformat
+git commit -am "Postlazy"
 
 for i in 0 1 2 3 4 5
 do
