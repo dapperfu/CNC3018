da575e4bcf3effd6fc347a18219942fea54a9ee8
Jed <jed@m6700.local>
2018-09-19 15:51:28 -0400

    .

--- tests/test_01.py
+++ tests/test_01.py
@@ -4,5 +4,18 @@ import GCode
 import GRBL
 
 
-def test_01():
-    print(uuid.uuid4())
+def test_01(session_uuid, module_uuid, class_uuid, function_uuid):
+    print("*"*50)
+    print("* Session UUID: {}".format(session_uuid))
+    print("* Module UUID: {}".format(module_uuid))
+    print("* Class UUID: {}".format(class_uuid))
+    print("* Function UUID: {}".format(function_uuid))
+    print("*"*50)
+
+def test_02(session_uuid, module_uuid, class_uuid, function_uuid):
+    print("*"*50)
+    print("* Session UUID: {}".format(session_uuid))
+    print("* Module UUID: {}".format(module_uuid))
+    print("* Class UUID: {}".format(class_uuid))
+    print("* Function UUID: {}".format(function_uuid))
+    print("*"*50)
