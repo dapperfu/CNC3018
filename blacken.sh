ea78481fe7a1704acd7867dc2149def593ffdc16
Jed <jed@m6700.local>
2018-09-20 00:09:14 -0400

    Prelazy

--- blacken.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while [ 1 ]
-do
-black --py36 --line-length 80 python_g*/*/*.py
-sleep 5
-done
--- lazy.sh
+++ lazy.sh
@@ -3,11 +3,5 @@
 # Make sure your venv is not a subfolder of where this is run.
 git commit -am "Prelazy"
 find . -name "*.py" | xargs -n1 -P8 black --py36 --line-length 80
-find . -name "*.ipynb" | xargs -n1 -P8 notebookformat
+find . -name "*.ipynb" | xargs -n1 -P8 notebookcleaner
 git commit -am "Postlazy"
-
-for i in 0 1 2 3 4 5
-do
-find . -name "*.py" | xargs -n1 -P8 isort --apply --multi-line $i
-git commit -am "isort multi-line: $i"
-done
